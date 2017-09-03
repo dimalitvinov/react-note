58f5489c7f706a0d6fa244d664eeb751c88d4fa9
dimalitvinov <dima8litvinov@gmail.com>
2017-09-03 13:12:44 +0400

    work addNote

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost
 
 var NotesContractABI = [{"constant":false,"inputs":[{"name":"_text","type":"bytes32"}],"name":"addNote","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"notes","outputs":[{"name":"text","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getNotes","outputs":[{"name":"","type":"bytes32[]"}],"payable":false,"type":"function"}];
 
-var NotesContractAddress = '0x0cad54b5382e4ee23af3288ebccff59ab7bb5e43';
+var NotesContractAddress = '0xb444e3fce758ec72e979cae5803cdeb819336ea6';
 var NotesContract = ETHEREUM_CLIENT.eth.contract(NotesContractABI).at(NotesContractAddress);
 
+// ETHEREUM_CLIENT.fromWei(ETHEREUM_CLIENT.eth.getBalance(ETHEREUM_CLIENT.eth.coinbase), "ether");
+// function checkAllBalances() {
+//     var totalBal = 0;
+//     for (var acctNum in ETHEREUM_CLIENT.eth.accounts) {
+//         var acct = ETHEREUM_CLIENT.eth.accounts[acctNum];
+//         var acctBal = ETHEREUM_CLIENT.fromWei(ETHEREUM_CLIENT.eth.getBalance(acct), "ether");
+//         totalBal += parseFloat(acctBal);
+//         console.log("  eth.accounts[" + acctNum + "]: \t" + acct + " \tbalance: " + acctBal + " ether");
+//     }
+//     console.log("  Total balance: " + totalBal + " ether");
+// };
+// checkAllBalances();
 
 class App extends Component {
 
@@ -21,21 +33,11 @@ class App extends Component {
   }
 
   componentWillMount() {
-    // var text = NotesContract.addNote('first note');
-    // console.log(text);
+    var result = NotesContract.addNote.sendTransaction('fifth note', {from: ETHEREUM_CLIENT.eth.accounts[0], gas: 3000000,});
     var data = NotesContract.getNotes();
     console.log(data);
 
-    // this.setState({
-    //     text: String(data[0]).split(','),
-    // });
   }
-  //
-  // pushNote(_text) {
-  //   NotesContract.addNote('first note');
-  //   //peopleContract.addPerson(/*_firstName, _lastName, _age*/'asdasda','asdasd',56);
-  //   //this.setState({deadline: this.state.newDeadline})
-  // };
 
   render() {
     return (
