f4fdbcd7907bc08a8232afcfde12e2211607f1f7
dimalitvinov <dima8litvinov@gmail.com>
2017-09-03 19:51:33 +0400

    fix getNotes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Web3 from 'web3';
 
 var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-var NotesContractABI = [{"constant":false,"inputs":[{"name":"_text","type":"bytes32"}],"name":"addNote","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"notes","outputs":[{"name":"text","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getNotes","outputs":[{"name":"","type":"bytes32[]"}],"payable":false,"type":"function"}];
+var NotesContractABI = [{"constant":false,"inputs":[{"name":"_text","type":"bytes32"}],"name":"addNote","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newText","type":"bytes32"}],"name":"editNote","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"notes","outputs":[{"name":"text","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getNotes","outputs":[{"name":"","type":"bytes32[]"}],"payable":false,"type":"function"}];
 
 var NotesContractAddress = '0xb444e3fce758ec72e979cae5803cdeb819336ea6';
 var NotesContract = ETHEREUM_CLIENT.eth.contract(NotesContractABI).at(NotesContractAddress);
@@ -23,20 +23,43 @@ var NotesContract = ETHEREUM_CLIENT.eth.contract(NotesContractABI).at(NotesContr
 // };
 // checkAllBalances();
 
+// var seconds = new Date().getTime() / 1000;
+
 class App extends Component {
 
   constructor(props) {
       super(props);
+
+      var data = NotesContract.getNotes();
+      var html = [];
+      for (var i = 0; i < data.length; i++) {
+        html.push(<div>{data[i]}</div>);
+      }
+
       this.state = {
           text: [],
+          allText: html,
       }
   }
 
-  componentWillMount() {
-    var result = NotesContract.addNote.sendTransaction('fifth note', {from: ETHEREUM_CLIENT.eth.accounts[0], gas: 3000000,});
-    var data = NotesContract.getNotes();
-    console.log(data);
+  getNotes() {
+      var data = NotesContract.getNotes();
+      var html = [];
+      for (var i = 0; i < data.length; i++) {
+        html.push(<div>{data[i]}</div>);
+      }
 
+      this.setState({allText: html});
+  }
+
+  componentDidMount() {
+      this.getNotes();
+  }
+
+  addNewNote() {
+      var result = NotesContract.addNote.sendTransaction(this.state.text, {from: ETHEREUM_CLIENT.eth.accounts[0], gas: 3000000,});
+      this.setState({text: this.state.allText})
+      this.getNotes();
   }
 
   render() {
@@ -44,11 +67,17 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React!!!</h2>
+          <h2>A NoteChain</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+            <input placeholder='New note' onChange={event => this.setState({text: event.target.value})}/>
+            <button onClick={() => this.addNewNote()}>
+                Submit
+            </button>
+        </div>
+        <div>
+            {this.state.allText}
+        </div>
       </div>
     );
   }
