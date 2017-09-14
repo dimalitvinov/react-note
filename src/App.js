a5c9c0df7483c64d589b8e84ecc84fcfc7970020
dimalitvinov <dima8litvinov@gmail.com>
2017-09-14 13:00:10 +0400

    add config

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import Item from './Item';
 import logo from './ethereum.png';
 import './App.css';
 import Web3 from 'web3';
-
-let ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-let NotesContractABI =
-[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"notes","outputs":[{"name":"id","type":"uint256"},{"name":"text","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_id","type":"uint256"},{"name":"_text","type":"bytes32"}],"name":"addNote","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"removeNote","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getNotes","outputs":[{"name":"","type":"uint256[]"},{"name":"","type":"bytes32[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"curId","type":"uint256"},{"name":"newText","type":"bytes32"}],"name":"editNote","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"}];
-let NotesContractAddress = '0xd6acf71ef15e6f7bbc09094fab7a6b0881126be5';
-let NotesContract = ETHEREUM_CLIENT.eth.contract(NotesContractABI).at(NotesContractAddress);
+import * as config from './config.js';
 
 class App extends Component {
 
@@ -37,14 +32,14 @@ class App extends Component {
     };
 
     editNote = field => {
-        let item = NotesContract.editNote.sendTransaction(field.id, field.text, {
-            from: ETHEREUM_CLIENT.eth.accounts[0],
+        let item = config.NotesContract.editNote.sendTransaction(field.id, field.text, {
+            from: config.ETHEREUM_CLIENT.eth.accounts[0],
             gas: 3000000
         });
     }
 
     getNotes = () => {
-        let rawNotes = NotesContract.getNotes(),
+        let rawNotes = config.NotesContract.getNotes(),
             listID = String(rawNotes[0]).split(','),
             listText = String(rawNotes[1]).split(',');
 
@@ -69,16 +64,16 @@ class App extends Component {
     }
 
     addNewNote() {
-        let result = NotesContract.addNote.sendTransaction(parseInt(Date.now()), this.state.newNote, {
-            from: ETHEREUM_CLIENT.eth.accounts[0],
+        let result = config.NotesContract.addNote.sendTransaction(parseInt(Date.now()), this.state.newNote, {
+            from: config.ETHEREUM_CLIENT.eth.accounts[0],
             gas: 3000000
         });
         this.getNotes();
     }
 
     removeNote(id) {
-        let item = NotesContract.removeNote.sendTransaction(id, {
-            from: ETHEREUM_CLIENT.eth.accounts[0],
+        let item = config.NotesContract.removeNote.sendTransaction(id, {
+            from: config.ETHEREUM_CLIENT.eth.accounts[0],
             gas: 3000000
         });
         this.getNotes();
